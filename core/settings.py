f2102083883d5070ad95f91f3dc8c36e0745d981
nepalimohan <nepalimohan@outlook.com>
2024-02-23 23:00:27 +0545

    chat_app views and urls

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'chat_app',
 ]
+
+LOGIN_URL = "/admin/login/"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
